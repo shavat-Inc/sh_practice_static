545fc6ece3b8f15af4c01d742232c9bedea72abc
shavatDesign <info@shavat.me>
2022-11-17 10:21:15 +0900

    save

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,68 +1,12 @@
-// mvパーティクル--------------------------------------
-new FinisherHeader({
-  "count": 9,
-  "size": {
-    "min": 480,
-    "max": 795,
-    "pulse": 0.5
-  },
-  "speed": {
-    "x": {
-      "min": 0.1,
-      "max": 1.0
-    },
-    "y": {
-      "min": 0.1,
-      "max": 0.6
+// 計算--------------------------------------
+function calc_total(){
+  kingaku = 0;
+  for (i=0; i<document.myform.length-1; i++){
+    if (document.myform.elements[i].checked){
+      kingaku += eval(document.myform.elements[i].value);
     }
-  },
-  "colors": {
-    "background": "#1a1a1a",
-    "particles": [
-      "#2062a2",
-      "#888888",
-      "#888888"
-    ]
-  },
-  "blending": "overlay",
-  "opacity": {
-    "center": 0.5,
-    "edge": 0.05
-  },
-  "skew": 0,
-  "shapes": [
-    "c"
-  ]
-});
+  }
+  document.myform.goukei.value = kingaku + "　円";
+}
 
-// マウスストーカー--------------------------------------
-
-//カーソル要素
-var cursor=$(".js-mouse-cursor");
-//ストーカー要素
-var stalker=$(".js-mouse-bg");
-
-$(document).on("mousemove",function(e){
-  var x=e.clientX;
-  var y=e.clientY;
-  cursor.css({
-    "top":y+"px",
-    "left":x+"px"
-  });
-  setTimeout(function(){
-    stalker.css({
-      "top":y+"px",
-      "left":x+"px"
-    });
-  });
-});
-
-$("a").on('mouseenter', function () {
-  cursor.addClass('is-active');
-  stalker.addClass('is-active');
-});
-
-$("a").on('mouseleave', function () {
-  cursor.removeClass('is-active');
-  stalker.removeClass('is-active');
-});
+console.log(document.myform.length);
